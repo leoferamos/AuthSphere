a910a3006d99164b8c0ee46ea13bb3b252f672f3
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-31 20:16:18 -0300

    feat: add CORS middleware configuration for API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import api
+from app.core.config.settings import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api.api_router, prefix="/api/v1")
