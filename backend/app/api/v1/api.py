d2ca9410b343340971925d6c933b385aba742326
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-31 20:57:11 -0300

    feat: include auth router in API routing configuration

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from .endpoints import users, form_fields
+from .endpoints import users, form_fields, auth 
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users")
-api_router.include_router(form_fields.router) 
+api_router.include_router(form_fields.router)
+api_router.include_router(auth.router) 
