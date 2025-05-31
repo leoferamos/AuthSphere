2ea7c04def8977b340b53b123e2506283f84ae2d
leoferamos <fernandesramosleonardo@gmail.com>
2025-05-31 14:17:38 -0300

    feat: update database configuration to remove default values for host and port

--- backend/app/core/config/settings.py
+++ backend/app/core/config/settings.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DB_USER: str
     DB_PASSWORD: str
-    DB_HOST: str = "localhost"
-    DB_PORT: int = 3306
+    DB_HOST: str
+    DB_PORT: int
     DB_NAME: str
     DB_SCHEMA: str | None = None  # Optional schema for MySQL
     DEBUG: bool = False
@@ -15,5 +15,12 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         case_sensitive = True
+
+    @property
+    def DB_URL(self):
+        return (
+            f"mysql+asyncmy://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?charset=utf8mb4"
+        )
 
 settings = Settings()
